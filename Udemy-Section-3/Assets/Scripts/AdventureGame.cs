28569f0e79dae1928a38e95e91c9bc40b1a441b3
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-10 22:29:58 -0600

    Section 3- started scifi text adventure game

---
+++ Udemy-Section-3/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+	// Use this for initialization
+	void Start () {
+        /*A “.” is within a component.Example: text with textComponent, or just textComponent.text*/
+        textComponent.text = ("You're in a long, brightly lit hallway. There are guards armed with light rifles on both sides of the hallway." +
+            "You are surrounded with no weapon. What do you do?");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Udemy-Section-3/Assets/Scripts/State.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State:ScriptableObject {
+
+    /*text area changes size of writable space in inspector*/
+
+    [TextArea(14,10)] [SerializeField] string storyText;
+
+}
