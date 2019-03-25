1a5cafd4629c512b10d510c0f235fdaff822d8b9
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-24 23:54:36 -0600

    Continued working on text adventure game

--- Udemy-Section-3/Assets/Scripts/AdventureGame.cs
+++ Udemy-Section-3/Assets/Scripts/AdventureGame.cs
@@ -6,16 +6,46 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour {
 
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    /*Array vvv
+    string[] daysWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };*/
+
+    State state; 
 
 	// Use this for initialization
 	void Start () {
-        /*A “.” is within a component.Example: text with textComponent, or just textComponent.text*/
-        textComponent.text = ("You're in a long, brightly lit hallway. There are guards armed with light rifles on both sides of the hallway." +
-            "You are surrounded with no weapon. What do you do?");
+        state = startingState;
+        /*A “.” is within a component.Example: text with textComponent, or just textComponent.text
+         state is the variable taht is allowing access to the State scriptand becaause we have access to the state class from the other script, can get access to GetStateStory*/
+        textComponent.text = state.GetStateStory();
+        /*This is telling unity to print out a thing from array above. The 3 tells it to print out the thing that is in the 3rd place in line, the 7 in this case
+        Debug.Log(daysWeek[3]);*/
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        ManageState();
 		
 	}
+
+    private void ManageState()
+    {
+        /* var is the same as saying State[], works with arrays*/
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        textComponent.text = state.GetStateStory();
+    }
 }
--- Udemy-Section-3/Assets/Scripts/State.cs
+++ Udemy-Section-3/Assets/Scripts/State.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State:ScriptableObject {
 
+
     /*text area changes size of writable space in inspector*/
 
     [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
 
 }
